bcf779c156e5dec7329f425db8fac2a126ad2375
mohamed amdouni <midouamdouni4@gmail.com>
2026-06-10 14:12:36 +0000

    basic pygame structure added

---
+++ app.py
@@ -0,0 +1,30 @@
+import pygame 
+import numpy as np
+import sys
+
+WINDOW_WIDTH=1280
+WINDOW_HEIGHT=720
+
+
+
+
+
+
+pygame.init()
+screen=pygame.display.set_mode(WINDOW_WIDTH,WINDOW_HEIGHT)
+pygame.display.flip("mohamed pyhsics engine")
+font=pygame.font.Font(None,36)
+green= (0, 255, 0)
+
+
+
+while True:
+    screen.fill(green)
+    for event in pygame.event.get():
+        if event==pygame.QUIT:
+            
+
+
+
+
+
