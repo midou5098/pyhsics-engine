ac7fe8da4fa46f4a0b3a89bcacb9a1838c5bb1ed
mohamed amdouni <midouamdouni4@gmail.com>
2026-06-10 14:18:26 +0000

    basic window working

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ WINDOW_HEIGHT=720
 
 
 pygame.init()
-screen=pygame.display.set_mode(WINDOW_WIDTH,WINDOW_HEIGHT)
-pygame.display.flip("mohamed pyhsics engine")
+screen=pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
+pygame.display.set_caption("mohamed pyhsics engine")
 font=pygame.font.Font(None,36)
 green= (0, 255, 0)
 
@@ -21,8 +21,11 @@ green= (0, 255, 0)
 while True:
     screen.fill(green)
     for event in pygame.event.get():
-        if event==pygame.QUIT:
-            
+        if event.type==pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+    pygame.display.flip()
+
 
 
 
