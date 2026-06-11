aceced005dc305ea4753e1695caade95535e68d3
mohamed amdouni <midouamdouni4@gmail.com>
2026-06-11 22:13:52 +0000

    sidebar pop is implemented

--- app.py
+++ app.py
@@ -17,12 +17,24 @@ pygame.init()
 screen=pygame.display.set_mode((WINDOW_WIDTH,WINDOW_HEIGHT))
 pygame.display.set_caption("mohamed pyhsics engine")
 font=pygame.font.Font(None,36)
+
+
+board=pygame.image.load("window.png").convert_alpha()
+board=pygame.transform.scale(board,(1200,680))
+
+
+
+
+
+
+
 beige=(200, 150, 120)
 essp=(80, 65, 55)
 esp_h=(10, 55, 80)
 green= (0, 255, 0)
 black= (0, 0, 0)
 menu=Button(1150,50,100,50,"menu",essp,esp_h)
+x=Button(50,50,50,50,"X",essp,esp_h)
 worldo=world(15,screen,8)
 obji=object(100,50,100,100,8000,1,0.7,False,pygame.Color('red'))
 objii=object(100,200,100,100,1500,1,0.7,False,pygame.Color('blue'))
@@ -47,11 +59,19 @@ while True:
             sys.exit()
         elif event.type==pygame.MOUSEBUTTONDOWN:
             worldo.pick(*event.pos)
+
+
         elif event.type==pygame.MOUSEBUTTONUP:
             worldo.release()
         elif event.type==pygame.MOUSEMOTION:
             worldo.set_mouse(*event.pos)
         menu.handle_event(event)
+        if menu.clicked:
+            popped=True
+        if popped:
+            x.handle_event(event)
+        if x.clicked:
+            popped=False
 
 
     menu.draw(screen)
@@ -59,7 +79,8 @@ while True:
     worldo.render()
     menu.draw(screen)
     if popped :
-        sbar.render(screen)
+        screen.blit(board,(20,20))
+        x.draw(screen)
     
     pygame.display.flip()
 
